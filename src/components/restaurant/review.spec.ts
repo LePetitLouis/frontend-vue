574d32ba9d14b06dc4fc91b55df03d52026064a6
Louis Poupat <louispoupat@sfr.fr>
2025-03-28 18:52:11 +0400

    feat: restaurant review unit test

--- src/components/restaurant/review.spec.ts
+++ src/components/restaurant/review.spec.ts
@@ -1,9 +1,24 @@
-// import { mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 
-// import RestaurantReview from './review.vue';
+import RestaurantReview from './review.vue';
+import { type Review } from '@/src/composables/restaurants';
+import restaurantsResponse from '@/mock/restaurants.json';
+
+const review = restaurantsResponse[0].reviews[0] as Review;
 
 describe(`restaurant-review`, () => {
-  test.todo(`no props`);
+  test(`no props`, (context) => {
+    const wrapper = mount(RestaurantReview, {
+      global: { plugins: [context.router] },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 
-  it.todo(`renders a review`);
+  it(`renders a review`, (context) => {
+    const wrapper = mount(RestaurantReview, {
+      global: { plugins: [context.router] },
+      props: { review },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 });
