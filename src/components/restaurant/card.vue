06166ce64d6e8804ee6899f417ffacf095b516b7
Louis Poupat <louispoupat@sfr.fr>
2025-03-28 14:47:42 +0400

    feat: display average rating

--- src/components/restaurant/card.vue
+++ src/components/restaurant/card.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import { type Restaurant } from '~/composables/restaurants';
+import type { Restaurant, Review } from '~/composables/restaurants';
 
-defineProps<{
+const props = defineProps<{
   restaurant: Restaurant
 }>();
+
+function formatAverage(num: number) {
+  const rounded = Math.round(num * 10) / 10;
+  return Number.isInteger(rounded) ? rounded : Number.parseFloat(rounded.toFixed(1));
+};
+
+const average = computed(() => {
+  const ratings = props.restaurant?.reviews?.map((review: Review) => review.rating);
+  if (!ratings || ratings.length === 0) return 0;
+  const sum = ratings.reduce((acc, rating) => acc + rating, 0);
+  return formatAverage(sum / ratings.length);
+});
 </script>
 
 <template>
@@ -16,11 +28,22 @@ defineProps<{
     <VCardTitle>
       {{ restaurant?.name }}
     </VCardTitle>
-    <VAlert variant="tonal" type="warning" class="mx-4 w-1/2">
+    <div v-if="average > 0" class="flex items-center gap-2 mx-4">
+      {{ average }}
+      <VRating
+        readonly
+        half-increments
+        :length="5"
+        :size="28"
+        :model-value="average"
+      />
+    </div>
+    <!-- TODO Fixed ! -->
+    <!-- <VAlert variant="tonal" type="warning" class="mx-4 w-1/2">
       TODO: display the average rating
       <br>
       Vuetify has a component for this. Use this one
-    </VAlert>
+    </VAlert> -->
     <VCardText>
       <RestaurantLocation :location="restaurant?.location" />
     </VCardText>
